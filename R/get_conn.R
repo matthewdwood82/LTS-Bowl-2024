91de3550d8d0c809480df4c1e9edb2e83e587592
matthewdwood82 <matthewdwood82@gmail.com>
2024-09-17 22:05:12 -0400

    update names for L2-4

--- R/get_conn.R
+++ R/get_conn.R
@@ -1,5 +1,5 @@
 lts_conn <- c("1132034136946069504", "1132034984149213184", "1132035064059207680", "1132035146527408128", "1132035221005737984") %>%
-  purrr::set_names("League 1", "League 2", "League 3", "League 4", "Leaghe 5") %>%
+  purrr::set_names("League 1", "L2 The Neverending Tilt Montage", "L3 The Only Legitimate LTS Bowl League", "L4 Sack Based 4", "League 5") %>%
   purrr::map(.x = .,
              ~ ffscrapr::ff_connect(
                league_id = .x,
