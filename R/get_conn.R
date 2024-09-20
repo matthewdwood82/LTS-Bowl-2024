137bf361f37d0b9ff94ab72dae7b7644718080e7
matthewdwood82 <matthewdwood82@gmail.com>
2024-09-20 09:14:12 -0400

    add remaining league names

--- R/get_conn.R
+++ R/get_conn.R
@@ -1,5 +1,17 @@
-lts_conn <- c("1132034136946069504", "1132034984149213184", "1132035064059207680", "1132035146527408128", "1132035221005737984") %>%
-  purrr::set_names("League 1", "L2 The Neverending Tilt Montage", "L3 The Only Legitimate LTS Bowl League", "L4 Sack Based 4", "League 5") %>%
+lts_conn <- c(
+  "1132034136946069504",
+  "1132034984149213184",
+  "1132035064059207680",
+  "1132035146527408128",
+  "1132035221005737984"
+) %>%
+  purrr::set_names(
+    "L1 Mo Alie-Cox Cup Memorial League",
+    "L2 The Neverending Tilt Montage",
+    "L3 The Only Legitimate LTS Bowl League",
+    "L4 Sack Based 4",
+    "L5 2024 TES Memorial Bidet Bowel"
+  ) %>%
   purrr::map(.x = .,
              ~ ffscrapr::ff_connect(
                league_id = .x,
